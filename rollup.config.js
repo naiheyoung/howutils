7780ef4466fed7f48ae16462b2431d3308d63cfe
Naihe <naiheemail@yeah.net>
2024-11-27 19:29:54 +0800

    chore: little

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,43 @@
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import esbuild from 'rollup-plugin-esbuild'
+import json from '@rollup/plugin-json'
+import clean from 'rollup-plugin-delete'
 import dts from 'rollup-plugin-dts'
+import pkg from './package.json' assert { type: 'json' }
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/index.d.ts',
-    format: 'esm',
+const input = 'src/index.ts'
+
+export default [
+  {
+    input,
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs'
+      },
+      {
+        file: pkg.module,
+        format: 'esm'
+      }
+    ],
+    plugins: [
+      clean({
+        targets: 'dist/*'
+      }),
+      nodeResolve(),
+      commonjs(),
+      json(),
+      esbuild({ target: 'esnext' })
+    ]
   },
-  plugins: [
-    dts({
-      respectExternal: true,
-    }),
-  ],
-}
+  {
+    input,
+    output: [
+      {
+        file: pkg.types
+      }
+    ],
+    plugins: [dts()]
+  }
+]
--- src/types.ts
+++ src/types.ts
@@ -3,4 +3,4 @@ interface DirectoryItem {
   child: DirectoryItem[]
 }
 
-type DeepPartial<T> = T extends object ? { [K in keyof T]?: DeepPartial<T[K]> } : T
+export type DeepPartial<T> = T extends object ? { [K in keyof T]?: DeepPartial<T[K]> } : T
